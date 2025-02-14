b8c0e1416e4e5582214ae821321d9063386694ba
ratnesh90859 <122274091+ratnesh90859@users.noreply.github.com>
2025-02-14 11:11:54 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://ratnesh90859:79857948@cluster0.8hkvsgu.mongodb.net/Foodie');
+        await mongoose.connect('');
         console.log("MongoDB Connected");
     } catch (error) {
         console.log("MongoDB Connection Error:", error);
